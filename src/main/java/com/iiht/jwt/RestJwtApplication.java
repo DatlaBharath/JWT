9c3f3e75817ccc16de9def0a47d07e1e084d1660
DatlaBharath <datlabharath92@gmail.com>
2024-08-13 21:28:40 +0530

    first commit

---
+++ src/main/java/com/iiht/jwt/RestJwtApplication.java
@@ -0,0 +1,12 @@
+package com.iiht.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJwtApplication.class, args);
+	}
+}
